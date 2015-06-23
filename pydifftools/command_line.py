211178b462e310bcd3e0516fc8780cd7d50f4c3f
John Franck <jmf356@cornell.edu>
2015-06-23 17:18:55 -0400

    added tools for word diff

--- pydifftools/command_line.py
+++ pydifftools/command_line.py
@@ -1,10 +1,12 @@
 import sys
 from . import check_numbers,match_spaces,split_conflict
+import os
 def errmsg():
     print """arguments are:
     num    (check numbers)
     wmatch (match whitespace)
-    sc     (split conflict)"""
+    sc     (split conflict)
+    wd     (word diff)"""
     return
 def main():
     if len(sys.argv) == 1:
@@ -17,6 +19,27 @@ def main():
         match_spaces.run(arguments)
     elif command == 'sc':
         split_conflict.run(arguments)
+    elif command == 'wd':
+        word_files = [x.replace('.md','.docx') for x in arguments[:2]]
+        for j in range(2):
+            cmd = ['pandoc']
+            cmd += [arguments[j]]
+            cmd += ['-s --smart']
+            if len(arguments) > 2:
+                if arguments[2][-5:] == '.docx':
+                    cmd += ['--reference-docx='+arguments[2]]
+                else:
+                    raise RuntimeError("if you pass three arguments to wd, then the third must be a template for the word document")
+            cmd += ['-o']
+            cmd += [word_files[j]]
+            print "about to run",' '.join(cmd)
+            os.system(' '.join(cmd))
+        cmd = ['start']
+        cmd += [os.path.expanduser('~/diff-doc.js')]
+        cmd += [os.getcwd() + os.path.sep + word_files[0]]
+        cmd += [os.getcwd() + os.path.sep + word_files[1]]
+        print "about to run",' '.join(cmd)
+        os.system(' '.join(cmd))
     else:
         errmsg()
     return
