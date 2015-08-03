5dad1796a3e4ad230d8f23b858456a731f3f30af
John Franck <jfranck@chem>
2015-08-03 15:01:42 -0400

    just changed gvim command

--- resolve_conflicts.sh
+++ resolve_conflicts.sh
@@ -2,12 +2,13 @@
 
 # diff is called by git with 7 parameters:
 # path old-file old-hex old-mode new-file new-hex new-mode
+gvimcmd="gvim"
 
 if [ "$#" == "0" ]; then
 	git status -s | sed "/^[^U]/d"
 else
     if ! [ -e $1.merge_head ]; then
-        python ~/notebook/split_conflict.py $1
+        pydifft sc $1
     fi
-	"/c/Program Files (x86)/Vim/vim74/gvim" -d $1.merge_head $1.merge_new
+	$gvimcmd -d $1.merge_head $1.merge_new
 fi
