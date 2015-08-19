af0b9c7e99d052e6da744bc7755ccb6a46643654
John Franck <jmf356@cornell.edu>
2015-08-19 16:03:44 -0400

    modified find task to use the -l command line parameter correctly

--- find_task.sh
+++ find_task.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # find a task -- like <c-g>o in vim
-if [ "$2" = "l" ] ; then
-    grep list* -rle "\\\\item\[.*\<$1\." | sed "/\.svn$/d" | sed "/\.swp$/d" | xargs -i@ "/c/Program Files/vim72/vim72/gvim.exe" -c 'exec("normal /\\\\item\\[.*\\<'$1'/e\nzO")' @
+if [ "$1" = "-l" ] ; then
+    grep list* -rle "\\\\item\[.*\<$2\." | sed "/\.svn$/d" | sed "/\.swp$/d" | xargs -i@ "/c/Program Files/vim72/vim72/gvim.exe" -c 'exec("normal /\\\\item\\[.*\\<'$2'/e\nzO")' @
 else
     grep inprocess/ *.tex -rle "\\label{sec:task$1}" | sed "/\/\./d" | sed "/\.svn$/d" | sed "/\.swp$/d" | xargs -i@ "/c/Program Files/vim72/vim72/gvim.exe" -c 'exec("normal /\\\\label{sec:task'$1'}\nzO")' @
 fi
