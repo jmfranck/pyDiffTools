ab157cc70a95b1defc66ae28a95d0bbfa5b1c97f
John Franck <jmf356@cornell.edu>
2015-11-15 20:32:01 -0500

    some debugging for the sentence wrapping

--- pydifftools/wrap_sentences.py
+++ pydifftools/wrap_sentences.py
@@ -10,17 +10,32 @@ def run(arguments,
     alltext = alltext.decode('utf-8')
     alltext = alltext.split('\n\n') #split paragraphs
     for para in range(len(alltext)):# split paragraphs into sentences
-        alltext[para] = re.split('([^\.!?]*[\.!?]) ',alltext[para])
+        #{{{ here I need a trick to prevent including short abbreviations, etc
+        tempsent = re.split('([^\.!?]{3}[\.!?])[ \n]',alltext[para])
+        for j in tempsent:
+            print "--",j
+        #{{{ put the "separators together with the preceding
+        alltext[para] = []
+        for tempsent_num in range(0,len(tempsent),2):
+            if tempsent_num+1 < len(tempsent):
+                alltext[para].append(tempsent[tempsent_num] + tempsent[tempsent_num+1])
+            else:
+                alltext[para].append(tempsent[tempsent_num])
+        print '-------------------'
+        for j in alltext[para]:
+            print "--",j
+        #}}}
+        #}}}
         for sent in range(len(alltext[para])):# sentences into words
             alltext[para][sent] = [word for word in re.split('[ \n]+',alltext[para][sent]) if len(word) > 0]
-        #{{{ join any sentences that end in know abbrevs
-        sent = 0
-        while sent < len(alltext[para]):
-            if len(alltext[para][sent]) > 0:
-                if alltext[para][sent][-1] in ['etc.','al.','vs.'] and sent < len(alltext[para]):
-                    alltext[para][sent:sent+1] = [' '.join(alltext[para][sent:sent+1])]
-            sent += 1
-        #}}}
+        ##{{{ join any sentences that end in know abbrevs
+        #sent = 0
+        #while sent < len(alltext[para]):
+        #    if len(alltext[para][sent]) > 0:
+        #        if alltext[para][sent][-1] in abbreviations and sent < len(alltext[para]):
+        #            alltext[para][sent:sent+1] = [alltext[para][sent] + alltext[para][sent+1]]
+        #    sent += 1
+        ##}}}
     lines = []
     for para in range(len(alltext)):# split paragraphs into sentences
         lines += ['\n'] # the extra line break between paragraphs
@@ -31,7 +46,7 @@ def run(arguments,
                 numchars = array(map(len,residual_sentence)) + 1 #+1 for space
                 cumsum_num = cumsum(numchars)
                 nextline_upto = argmin(abs(cumsum_num - wrapnumber))
-                nextline_punct_upto = array([cumsum_num[j] if residual_sentence[j][-1]==',' else 10000 for j in range(len(residual_sentence)) ])
+                nextline_punct_upto = array([cumsum_num[j] if (residual_sentence[j][-1] in [u',',u';',u':',u')',u'-']) else 10000 for j in range(len(residual_sentence)) ])
                 if any(nextline_punct_upto < 10000):
                     nextline_punct_upto = argmin(abs(nextline_punct_upto - wrapnumber))
                     if nextline_punct_upto < nextline_upto:
