d5a4f5a4435569fc724172d9c1218d079acb259b
John Franck <jmf356@cornell.edu>
2016-02-22 10:16:54 -0500

    change find task so that vim is a variable

--- find_task.sh
+++ find_task.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # find a task -- like <c-g>o in vim
+vimlocation="/c/Program Files/vim72/vim72/gvim.exe"
 if [ "$1" = "-l" ] ; then
     if [ `echo $2 |sed -n '/^[0-9]\+$/p'` ] ; then
         echo "this is a number"
-        grep list* -rle "\\\\item\[.*\<$2\." | sed "/\.svn$/d" | sed "/\.swp$/d" | xargs -i@ "/c/Program Files/vim72/vim72/gvim.exe" -c 'exec("normal /\\\\item\\[.*\\<'$2'\\./e\nzO")' @
+        grep list* -rle "\\\\item\[.*\<$2\." | sed "/\.svn$/d" | sed "/\.swp$/d" | xargs -i@ "$vimlocation" -c 'exec("normal /\\\\item\\[.*\\<'$2'\\./e\nzO")' @
     else
         echo "this is not a number"
         vimsearch='exec("normal /\\<'$2'\\>\nzO")'
         echo "I find the following files that match"
         echo `grep list* -rle "\<$2\>" | sed -n "/\.tex$/p"`
         echo "and I plan to run the following vim command $vimsearch"
-        grep list* -rle "\<$2\>" | sed -n "/\.tex$/p" | xargs -i@ "/c/Program Files/vim72/vim72/gvim.exe" -c "$vimsearch" @ # apparently the quotes are required to expand the $vimsearch here -- this is equivalent to typing the lhs of the equation above here
+        grep list* -rle "\<$2\>" | sed -n "/\.tex$/p" | xargs -i@ "$vimlocation" -c "$vimsearch" @ # apparently the quotes are required to expand the $vimsearch here -- this is equivalent to typing the lhs of the equation above here
     fi
 else
     testresult=$(grep inprocess/ *.tex -rle "\\label{sec:task$1}" | sed -n '/\.tex\|\.txt/p')
     if [ "$testresult" ] ; then
-        echo "$testresult" | xargs -i@ "/c/Program Files/vim72/vim72/gvim.exe" -c 'exec("normal /\\\\label{sec:task'$1'}\nzO")' @
+        echo "$testresult" | xargs -i@ "$vimlocation" -c 'exec("normal /\\\\label{sec:task'$1'}\nzO")' @
     else
         echo "I can't find $1 in the notebook"
     fi
