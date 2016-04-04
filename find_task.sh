a77354b2ce412e4f71ee6dd33bdc1ad33c29fa4e
John Franck <jmf356@cornell.edu>
2016-04-04 17:18:39 -0400

    updates to find task

--- find_task.sh
+++ find_task.sh
@@ -1,23 +1,52 @@
 #!/bin/sh
 # find a task -- like <c-g>o in vim
 vimlocation="/c/Program Files/vim72/vim72/gvim.exe"
-if [ "$1" = "-l" ] ; then
-    if [ `echo $2 |sed -n '/^[0-9]\+$/p'` ] ; then
+cd ~/notebook
+
+# process arguments -- modified from http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+find_mode="vim"
+while [[ $# > 1 ]]
+do
+key="$1"
+
+    case $key in
+        -l|--listnum)
+            find_mode="list"
+            shift # $2 --> $1, $3 --> $2, etc.
+        ;;
+        -p|--print)
+            find_mode="print"
+            shift # past argument
+        ;;
+        *)
+            echo "I don't know what $key is supposed to do!"
+            exit 1 # unknown option
+        ;;
+    esac
+done
+input_str="$1"
+
+if [ "$find_mode" = "list" ] ; then
+    if [ `echo $input_str |sed -n '/^[0-9]\+$/p'` ] ; then
         echo "this is a number"
-        grep list* -rle "\\\\item\[.*\<$2\." | sed "/\.svn$/d" | sed "/\.swp$/d" | xargs -i@ "$vimlocation" -c 'exec("normal /\\\\item\\[.*\\<'$2'\\./e\nzO")' @
+        grep list* -rle "\\\\item\[.*\<$input_str\." | sed "/\.svn$/d" | sed "/\.swp$/d" | xargs -i@ "$vimlocation" -c 'exec("normal /\\\\item\\[.*\\<'$input_str'\\./e\nzO")' @
     else
         echo "this is not a number"
-        vimsearch='exec("normal /\\<'$2'\\>\nzO")'
+        vimsearch='exec("normal /\\c\\<'$input_str'\\>\nzO")'
         echo "I find the following files that match"
-        echo `grep list* -rle "\<$2\>" | sed -n "/\.tex$/p"`
+        echo `grep list* -rile "\<$input_str\>" | sed -n "/\.tex$/p"`
         echo "and I plan to run the following vim command $vimsearch"
-        grep list* -rle "\<$2\>" | sed -n "/\.tex$/p" | xargs -i@ "$vimlocation" -c "$vimsearch" @ # apparently the quotes are required to expand the $vimsearch here -- this is equivalent to typing the lhs of the equation above here
+        grep list* -rile "\<$input_str\>" | sed -n "/\.tex$/p" | xargs -i@ "$vimlocation" -c "$vimsearch" @ # apparently the quotes are required to expand the $vimsearch here -- this is equivalent to typing the lhs of the equation above here
     fi
 else
-    testresult=$(grep inprocess/ *.tex -rle "\\label{sec:task$1}" | sed "/\.swp$/d" | sed -n '/\.tex\|\.txt/p')
+    testresult=$(grep inprocess/ *.tex -rle "\\label{sec:task$input_str}" | sed -n '/\.tex\|\.txt/p')
     if [ "$testresult" ] ; then
-        echo "$testresult" | xargs -i@ "$vimlocation" -c 'exec("normal /\\\\label{sec:task'$1'}\nzO")' @
+        if [ "$find_mode" = "vim" ]; then
+            echo "$testresult" | xargs -i@ "$vimlocation" -c 'exec("normal /\\\\label{sec:task'$input_str'}\nzO")' @
+        elif [ "$find_mode" = "print" ]; then
+            echo "$testresult"
+        fi
     else
-        echo "I can't find $1 in the notebook"
+        echo "I can't find $input_str in the notebook"
     fi
 fi
