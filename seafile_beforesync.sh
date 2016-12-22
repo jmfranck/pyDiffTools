bbe8ecf05f0b25b8d7def57f2e6257fec739e24e
John Franck <jmf356@cornell.edu>
2016-12-22 00:03:53 -0500

    changes to seafile script

--- seafile_beforesync.sh
+++ seafile_beforesync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 echo "if you think the files from seafile are newer, then delete the corresponding SFConflict file before running this"
-ls *SFConflict* | sed 's/\(.*\)\( (SFConflict.*)\)\.\([a-z_]\+\)/mv "\1\2.\3" \1.\3/' > temp.sh
-echo """
+find . -name "*SFConflict*" | sed 's/\(.*\)\( (SFConflict.*)\)\.\([a-z_]\+\)/mv "\1\2.\3" \1.\3/' > temp.sh
+echo ""
 echo "after running this, check and run temp.sh to restore to the files that were in use on this computer"
