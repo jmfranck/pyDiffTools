0aaac41127cf783232fbc3b7d2c7e394d2103deb
John Franck <jmf356@cornell.edu>
2017-05-07 14:32:28 -0400

    update find task to use ctag (auto-updated by git hooks) where relevant

--- find_task.sh
+++ find_task.sh
@@ -29,7 +29,7 @@ input_str="$1"
 if [ "$find_mode" = "list" ] ; then
     if [ `echo $input_str |sed -n '/^[0-9]\+$/p'` ] ; then
         echo "this is a number"
-        grep list* -rle "\\\\item\[.*\<$input_str\." | sed "/\.svn$/d" | sed "/\.swp$/d" | xargs -n 1 -P 5 -i@ "$vimlocation" -c 'exec("normal /\\\\item\\[.*\\<'$input_str'\\./e\nzO")' @
+        gvim -t "lst:$input_str" &
     else
         echo "this is not a number"
         vimsearch='exec("normal /\\c\\<'$input_str'\\>\nzO")'
@@ -39,12 +39,19 @@ if [ "$find_mode" = "list" ] ; then
         grep list* -rile "\<$input_str\>" | sed -n "/\.tex$/p" | xargs -n 1 -P 5 -i@ "$vimlocation" -c "$vimsearch" @ # apparently the quotes are required to expand the $vimsearch here -- this is equivalent to typing the lhs of the equation above here
     fi
 else
-    testresult=$(grep inprocess/ *.tex -rle "\\label{sec:task$input_str}" | sed "/\.swp$/d" | sed -n '/\.tex\|\.txt/p')
-    if [ "$testresult" ] ; then
-        if [ "$find_mode" = "vim" ]; then
+    if [ "$find_mode" = "vim" ]; then
+        if [ `echo $input_str |sed -n '/^[0-9]\+$/p'` ] ; then
+            echo "this is a number"
+            gvim -t "sec:task$input_str" &
+        else
             echo "$testresult" | xargs -n 1 -P 5 -i@ "$vimlocation" -c 'exec("normal /\\\\label{sec:task'$input_str'}\nzO")' @
-        elif [ "$find_mode" = "print" ]; then
+        fi
+    elif [ "$find_mode" = "print" ]; then
+        testresult=$(grep inprocess/ *.tex -rle "\\label{sec:task$input_str}" | sed "/\.swp$/d" | sed -n '/\.tex\|\.txt/p')
+        if [ "$testresult" ] ; then
             echo "$testresult"
+        else
+            echo "I can't find $input_str in the notebook"
         fi
     else
         echo "I can't find $input_str in the notebook"
